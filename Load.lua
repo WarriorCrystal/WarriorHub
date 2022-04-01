56af7b632620e2525abde9a828cc975a8eddff26
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-04-01 20:03:38 -0300

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 662417684 then
-loadstring(game:HttpGet("https://pastebin.com/raw/kzvMP4Zj"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Lucky%20Block%20Battlegrounds.lua"))()
 end
