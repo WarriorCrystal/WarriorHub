b1d0fae5e04ef933b86b999b15b68ebc79d2c415
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-04-02 14:29:36 -0300

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,4 +1,8 @@
 local inviteModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+inviteModule.Prompt({
+name = "WarriorHub",
+invite = "https://discord.gg/AvjPbX6wcn",
+})
 if game.PlaceId == 662417684 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Lucky%20Block%20Battlegrounds.lua"))()
 end
@@ -6,7 +10,4 @@ if game.PlaceId == 4787629450 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Booga%20Booga.lua"))()
 end
 
-inviteModule.Prompt({
-name = "WarriorHub",
-invite = "https://discord.gg/AvjPbX6wcn",
-})
+
