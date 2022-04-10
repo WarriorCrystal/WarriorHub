e04b46246bccd7ec276ccc2eb4f2dea5143321b6
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-04-10 18:23:37 -0300

    Update Booga Booga V2.lua

--- Booga Booga V2.lua
+++ Booga Booga V2.lua
@@ -117,6 +117,92 @@ Main:Toggle("Auto Mine Mag (Put pick on slot 2)",false, function(bool)
         loop = false
     end
 end)
+Main:Button("Place Chest Campfire", function()
+    local Fires = 1
+    local Event = game:GetService("ReplicatedStorage").Events.PlaceStructure
+    h = game.Players.LocalPlayer.Character.LowerTorso.Position
+    local c = h.x + 5
+    local d = h.y - 2
+    local e = h.z
+    local j = h.x
+    local k = h.y - 2
+    local l = h.z
+    local aa = h.x - 5
+    local bb = h.y - 2
+    local cc = h.z
+    local aaa = h.x
+    local bbb = h.y - 2
+    local ccc = h.z + 5
+    local aaaa = h.x
+    local bbbb = h.y - 2
+    local cccc = h.z - 5
+    for i = 1,1 do
+        local C_1 = "Chest"
+        local C_2 = CFrame.new(j, k, l, 1, 0, 0, 0, 1, -5.56028681e-08, 0, -5.56028681e-08, 1)
+        local C_3 = 0
+        Event:FireServer(C_1, C_2, C_3)
+    end
+    for i = 1,Fires do
+        local A_1 = "Campfire"
+        local A_2 = CFrame.new(c, d, e, 1, 0, 0, 0, 1, -5.56028681e-08, 0, -5.56028681e-08, 1)
+        local A_3 = 0
+        Event:FireServer(A_1, A_2, A_3)
+    end
+    for i = 1,Fires do
+        local B_1 = "Campfire"
+        local B_2 = CFrame.new(aa, bb, cc, 1, 0, 0, 0, 1, -5.56028681e-08, 0, -5.56028681e-08, 1)
+        local B_3 = 0
+        Event:FireServer(B_1, B_2, B_3)
+    end
+    for i = 1,Fires do
+        local D_1 = "Campfire"
+        local D_2 = CFrame.new(aaa, bbb, ccc, 1, 0, 0, 0, 1, -5.56028681e-08, 0, -5.56028681e-08, 1)
+        local D_3 = 0
+        Event:FireServer(D_1, D_2, D_3)
+    end
+    for i = 1,Fires do
+        local E_1 = "Campfire"
+        local E_2 = CFrame.new(aaaa, bbbb, cccc, 1, 0, 0, 0, 1, -5.56028681e-08, 0, -5.56028681e-08, 1)
+        local E_3 = 0
+        Event:FireServer(E_1, E_2, E_3)
+    end
+end)
+Main:Button("Auto Wall Trap", function()
+    local Event = game:GetService("ReplicatedStorage").Events.PlaceStructure
+    h = game.Players.LocalPlayer.Character.LowerTorso.Position
+    local aa = h.x + 13
+    local bb = h.y - 2
+    local cc = h.z
+    local aaa = h.x - 13
+    local bbb = h.y - 2
+    local ccc = h.z
+    local aaaa = h.x
+    local bbbb = h.y - 2
+    local cccc = h.z + 13
+    local aaaaa = h.x
+    local bbbbb = h.y - 2
+    local ccccc = h.z - 13
+    local k_1 = "Stone Wall"
+    local k_2 = CFrame.new(aaaa, bbbb, cccc, -0.99862951, 9.53660761e-12, 0.0523360483, -9.54969524e-12, 1, 2.71050543e-20, -0.0523360483, 4.99793241e-13, -0.99862951)
+    local k_3 = 177
+    local Event = game:GetService("ReplicatedStorage").Events.PlaceStructure
+    Event:FireServer(k_1, k_2, k_3)
+    local l_1 = "Stone Wall"
+    local l_2 = CFrame.new(aaaaa, bbbbb, ccccc, -0.99862951, 9.53660761e-12, 0.0523360483, -9.54969524e-12, 1, 2.71050543e-20, -0.0523360483, 4.99793241e-13, -0.99862951)
+    local l_3 = 177
+    local Event = game:GetService("ReplicatedStorage").Events.PlaceStructure
+    Event:FireServer(l_1, l_2, l_3)
+    local m_1 = "Stone Wall"
+    local m_2 = CFrame.new(aaa, bbb, ccc, 1.19248806e-08, 3.87430191e-06, -1, 4.62005888e-14, 1, -3.87430191e-06, 1, 0, 1.19248806e-08)
+    local m_3 = 270
+    local Event = game:GetService("ReplicatedStorage").Events.PlaceStructure
+    Event:FireServer(m_1, m_2, m_3)
+    local n_1 = "Stone Wall"
+    local n_2 = CFrame.new(aa, bb, cc, 1.19248806e-08, 3.87430191e-06, -1, 4.62005888e-14, 1, -3.87430191e-06, 1, 0, 1.19248806e-08)
+    local n_3 = 270
+    local Event = game:GetService("ReplicatedStorage").Events.PlaceStructure
+    Event:FireServer(n_1, n_2, n_3)
+end)
 Main:Textbox("Tp Spam", "Write victim name", true, function(t)
     for i,v in pairs(game:GetService("Players"):GetChildren()) do
         if v.Name:lower():find(t:lower()) then
@@ -132,6 +218,8 @@ Main:Textbox("Tp Spam", "Write victim name", true, function(t)
         end
     end
 end)
+
+
 
 
 local Crafting = serv:Channel("Crafting")
