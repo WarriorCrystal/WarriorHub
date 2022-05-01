ef22e0764ee9be24e258aaed0590bab6a2e9d81c
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-04-30 22:47:13 -0300

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -4,5 +4,6 @@ end
 if game.PlaceId == 4787629450 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Booga%20Booga%20V3.lua"))()
 end
-
-
+if game.PlaceId ~= 4787629450 or game.PlaceId ~= 662417684 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Universal.lua"))()
+end
