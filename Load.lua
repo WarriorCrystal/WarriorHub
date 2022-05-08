881de0573b6e3997866f8acac045bb3bbfe4b835
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-05-08 09:52:51 -0300

    Re-Added Universal, added Discord Prompt

--- Load.lua
+++ Load.lua
@@ -1,9 +1,11 @@
+local inviteModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+inviteModule.Prompt({ name = "Warrior Hub", invite = "https://discord.gg/EdHhqP7TzZ", })
 if game.PlaceId == 662417684 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Lucky%20Block%20Battlegrounds.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Lucky%20Block%20Battlegrounds.lua"))()
 end
 if game.PlaceId == 4787629450 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Booga%20Booga%20V3.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Booga%20Booga%20V3.lua"))()
 end
--- if game.PlaceId ~= 4787629450 and game.PlaceId ~= 662417684 then
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Universal.lua"))()
--- end
+if game.PlaceId ~= 4787629450 and game.PlaceId ~= 662417684 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Universal.lua"))()
+end
