812275b746a7083683afebe2fb3d58344b7745aa
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2022-06-21 10:52:51 -0300

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -18,6 +18,9 @@ end
 if game.PlaceId == 9500891570 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Admin%20Guns.lua"))()
 end
-if game.PlaceId ~= 4787629450 and game.PlaceId ~= 662417684 and game.PlaceId ~= 9203864304 and game.PlaceId ~= 5901346231 and game.PlaceId ~= 9312552871 and game.PlaceId ~= 9500891570 then
+if game.PlaceId == 8131316275 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Booga%20Booga%202019.lua"))()
+end
+if game.PlaceId ~= 4787629450 and game.PlaceId ~= 662417684 and game.PlaceId ~= 9203864304 and game.PlaceId ~= 5901346231 and game.PlaceId ~= 9312552871 and game.PlaceId ~= 8131316275 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WarriorCrystal/WarriorHub/main/Universal.lua"))()
 end
